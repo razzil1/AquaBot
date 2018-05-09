6a5ccc6b8c8916ba53c1b6df9c417844a1b21512
Razzil1 <razzil1@yahoo.com>
2018-05-09 13:06:12 +0200

    first init

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+'use strict'
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const verificationController = require('./controllers/verification');
-const messageWebhookController = require('./controllers/messageWebhook');
-
+const request = require('request');
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Process application/json
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', verificationController);
-app.post('/', messageWebhookController);
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a chat bot')
+});
 
-app.set('port', (process.env.PORT || 5000))
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+});
+
+// Spin up the server
 app.listen(app.get('port'), function() {
-  console.log('running on port', app.get('port'))
+	console.log('running on port', app.get('port'))
 });
