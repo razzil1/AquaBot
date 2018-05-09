5a56a6d83bba042788c78afa8c0e3f46ea412cac
Razzil1 <razzil1@yahoo.com>
2018-05-09 13:11:44 +0200

    sendTextMessage

--- index.js
+++ index.js
@@ -26,7 +26,41 @@ app.get('/webhook/', function (req, res) {
 	res.send('Error, wrong token')
 });
 
+app.post('/webhook/', function (req, res) {
+  let messaging_events = req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) {
+      let text = event.message.text
+      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+    }
+  }
+  res.sendStatus(200)
+})
+
+const token = "EAACm6tf2U7QBAD5yr4vL33wRHL5Ljt2xZAiXbj83a94X1gQz7SvMWgOhw7IWeZAdZBZBfmqzNwZCI8N5uaOMLmkThuMxlDiEUm5E8qncq31RhTR0yIIszRbzBqAU0vdZCzGYVrq5S6wUiLOq9dvwZC5ejZCSwPprvxXERvFuac9LyQULziDRAbpS";
+
 // Spin up the server
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 });
+
+function sendTextMessage(sender, text) {
+  let messageData = { text:text }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+  json: {
+      recipient: {id:sender},
+    message: messageData,
+  }
+}, function(error, response, body) {
+  if (error) {
+      console.log('Error sending messages: ', error)
+  } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
