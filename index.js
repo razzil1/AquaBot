c58a0c3286d03c52a3100cc976d583bb22d922e6
Razzil1 <razzil1@yahoo.com>
2018-05-09 12:35:16 +0200

    new project2

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const bodyParser = require('body-parser');
 const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
 
-app.get('/', verificationController);
-app.post('/', messageWebhookController);
-
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', verificationController);
+app.post('/', messageWebhookController);
 
 app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'), function() {
