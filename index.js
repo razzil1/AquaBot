dd410cae48d46232f45e051f2625282193336c0f
Razzil1 <razzil1@yahoo.com>
2018-05-11 17:11:07 +0200

    removed tolowercase

--- index.js
+++ index.js
@@ -44,12 +44,8 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id
     if (event.message && event.message.text) {
       let text = event.message.text
-      if (text.toString().toLowerCase().contains('hi') || text.toString().toLowerCase().contains('hello')) {
-        sendTextMessage(sender, "Hi there!");
-        continue;
-      }
 
-      if (text.toString().toLowerCase() === 'unsubscribe') {
+      if (text === 'Unsubscribe') {
         removeUser(sender);
         continue;
       }
