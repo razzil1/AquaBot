9129e6165cf773168c8fc99c775c4b35be005f6d
Razzil1 <razzil1@yahoo.com>
2018-05-11 16:46:26 +0200

    changing messages

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/webhook/', function (req, res) {
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
-      sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+      sendTextMessage(sender, "Postback received: "+text.substring(0, 200))
       continue
     }
   }
