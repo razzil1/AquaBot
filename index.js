d3d563403874ae1dff3aa685e03a06b5e75e9b6d
Razzil1 <razzil1@yahoo.com>
2018-05-11 17:07:12 +0200

    toString

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id
     if (event.message && event.message.text) {
       let text = event.message.text
-      if (text.toLowerCase().contains('hi') || text.toLowerCase().contains('hello')) {
+      if (text.toString().toLowerCase().contains('hi') || text.toString().toLowerCase().contains('hello')) {
         sendTextMessage(sender, "Hi there!");
         continue;
       }
