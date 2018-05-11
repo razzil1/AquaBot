38125dd9ce6638fec7a5e99d5f7fc55ee8f5e600
Razzil1 <razzil1@yahoo.com>
2018-05-11 17:09:03 +0200

    toString

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post('/webhook/', function (req, res) {
         continue;
       }
 
-      if (text.toLowerCase() === 'unsubscribe') {
+      if (text.toString().toLowerCase() === 'unsubscribe') {
         removeUser(sender);
         continue;
       }
